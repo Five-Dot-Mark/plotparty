39440a5ea62eb1a46afac779a4fb6583567e2a7c
Andrey Demenev <andrey@1crm.com>
2025-08-27 22:43:35 +0400

    molnar: paramatrize the sketch

--- molnar/sketch_molnar.py
+++ molnar/sketch_molnar.py
@@ -2,31 +2,32 @@ import vsketch
 
 
 class MolnarSketch(vsketch.SketchClass):
-    # Sketch parameters:
-    
+    scale_to_bottle = vsketch.Param(False)
+    spacing = vsketch.Param(3.5, decimals=2)
+    cells_x = vsketch.Param(4)
     def draw(self, vsk: vsketch.Vsketch) -> None:
-        vsk.size("320mm", "170mm", landscape=False)
+        height = 170
+        vsk.size("320mm" if self.scale_to_bottle else "220mm", f"{height}mm", landscape=False)
         vsk.scale("mm")
-        vsk.scale(320.0 / 220, 1)
+        if self.scale_to_bottle:
+            vsk.scale(320.0 / 220, 1)
 
-        penWidth = 1.2
-        numCellsX = 4
-        numCellsY = 3
+        numCellsX = self.cells_x
         cellSize = 220.0 / numCellsX
-        rectSize = cellSize - penWidth * 2
+        numCellsY = int(height / cellSize)
+        rectSize = cellSize - self.spacing
 
-        #implement your sketch here
         for x in range(0, numCellsX):
             for y in range(0, numCellsY):
                 size = rectSize
-                while size > penWidth * 3:
+                while size > self.spacing:
                     vsk.pushMatrix()
                     vsk.translate(x * cellSize, y * cellSize)
-                    #vsk.rotate(vsk.random(-0.05, 0.05))
+                    vsk.rotate(vsk.random(-0.05 * y / 4, 0.05 * y / 4))
                     if vsk.random(1) > 0.35:
                         vsk.rect(- size / 2, - size/2, size, size)
                     vsk.popMatrix()
-                    size -= penWidth*3
+                    size -= self.spacing
 
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
